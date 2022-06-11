6eaa7b9c4e1ad2a709425b897de974eddbd2dabc
amiturfriend007 <amiturfriend007@yahoo.com>
2022-06-11 16:06:39 +0530

    [Avenger]: First Commit

---
+++ spring-client/src/main/java/com/example/springclient/SpringClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.springclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.EnableConfigServer;
+@SpringBootApplication
+//@EnableConfigServer
+public class SpringClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClientApplication.class, args);
+    }
+
+}
